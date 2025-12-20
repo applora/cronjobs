7b3094ad4e81f4a7caedfb51e1b4a72da2558030
yuangwei <yuang.wya@gmail.com>
2025-12-20 12:43:47 +0800

    feat: update

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import "dotenv/config"
-import { serve } from '@hono/node-server'
-import app from './api/index.js'
+import "dotenv/config";
+import { serve } from "@hono/node-server";
+import app from "./api/index.js";
 
-serve({
-  fetch: app.fetch,
-  port: 3000,
-}, (info) => {
-  console.log(`Server start at: http://localhost:${info.port}`)
-})
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server start at: http://localhost:${info.port}`);
+  }
+);
